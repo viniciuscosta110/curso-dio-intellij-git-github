7d5d44d165cf14dd451d36d35b43a1c7525cc30a
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2022-04-30 00:33:06 -0300

    First commit

---
+++ src/br/com/dio/FirstProgram.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Cat;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        /*int a = 5;
+        int b = 3;
+
+        System.out.println("Hello World! " + (a+b) );*/
+
+        Cat cat = new Cat();
+
+        System.out.println(cat);
+
+
+    }
+}
